df516eed1789cd661e88540aafa8193ca31d09c2
Dinesh-M-India <chandru08112000@gmail.com>
2023-08-02 17:07:43 +0530

    Update package.json and Hero.js, add Image-4.png

--- src/components/home_component.js/Hero.js
+++ src/components/home_component.js/Hero.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "../../styles/style.css";
 import Linkedin from "../../assets/image-3.png";
